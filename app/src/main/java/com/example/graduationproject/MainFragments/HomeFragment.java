da6da0871d0d13571adf3c2274c84e2eac761c01
Muhammad <https://github.com/Mu7hammad/Graduation_project>
2022-03-28 14:34:20 +0200

    This is the first update

--- app/src/main/java/com/example/graduationproject/MainFragments/HomeFragment.java
+++ app/src/main/java/com/example/graduationproject/MainFragments/HomeFragment.java
@@ -3,42 +3,16 @@ package com.example.graduationproject.MainFragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import com.example.graduationproject.R;
-import com.example.graduationproject.SubFragments.ResultFragment;
-
 public class HomeFragment extends Fragment {
-
-    ImageView img_result;
-    ResultFragment resultFragment;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-
-        resultFragment = new ResultFragment();
-
-        img_result = view.findViewById(R.id.result);
-        img_result.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                fragmentTransaction.replace(R.id.container, resultFragment);
-                fragmentTransaction.addToBackStack(null);
-                fragmentTransaction.commit();
-
-            }
-        });
-
         return view;
     }
 }
